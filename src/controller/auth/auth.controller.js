50dde8f2da291479bb4a254c0ee0c1ab11997b51
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-05-11 23:13:53 +0300

    validate user's input, verify their password, generate token using jwt and send back the token for subsequent request, handle error gracefully

--- src/controller/auth/auth.controller.js
+++ src/controller/auth/auth.controller.js
@@ -2,7 +2,10 @@ import { StatusCodes } from "http-status-codes";
 import transactionsHelper from "../../utils/mongoose.transaction.helper.js";
 import User from "../../models/user.model.js";
 import APIError from "../../utils/ApiError.js";
-import { userSchemaValidation } from "../../utils/schema-validation/userSchema.validation.js";
+import {
+  authSchema,
+  userSchemaValidation,
+} from "../../utils/schema-validation/userSchema.validation.js";
 
 async function signUp(req, res, next) {
   await transactionsHelper(async (session) => {
@@ -13,7 +16,8 @@ async function signUp(req, res, next) {
     if (error) {
       return next(
         APIError.badRequest(
-          error?.details[0]?.message || "Please provide valid Data",
+          error?.details[0]?.message ||
+            "Bad Request, Please provide valid Data",
         ),
       );
     }
@@ -44,10 +48,31 @@ async function signUp(req, res, next) {
   }, next);
 }
 
-async function signIn(req, res) {
-  res
-    .status(StatusCodes.OK)
-    .json({ success: true, message: "sign in success" });
+async function signIn(req, res, next) {
+  try {
+    const value = await authSchema.validateAsync({ ...req.body });
+
+    const user = await User.findOne(
+      { email: value.email },
+      "email password username role",
+      null,
+    );
+    if (!user) {
+      return next(APIError.unauthorized("Invalid Credential"));
+    }
+
+    if (!(await user.passwordVerified(value.password))) {
+      return next(APIError.unauthorized());
+    }
+
+    const token = await user.getToken();
+
+    res
+      .status(StatusCodes.OK)
+      .json({ success: true, message: "sign in successful", token, user });
+  } catch (err) {
+    next(APIError.badRequest(err.message));
+  }
 }
 
 async function signOut(req, res) {
