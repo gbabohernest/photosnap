03207faab2855117e9bf912eb6d7a10a8e08d0a6
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-05-16 05:56:32 +0300

    upload handler - a function that handles file upload

--- src/controller/user/me/upload.controller.js
+++ src/controller/user/me/upload.controller.js
@@ -1,10 +1,39 @@
 import Image from "../../../models/image.model.js";
+import imageSchemaValidation from "../../../utils/schema-validation/imageSchema.validation.js";
+import APIError from "../../../utils/ApiError.js";
+import transactionsHelper from "../../../utils/mongoose.transaction.helper.js";
+import { StatusCodes } from "http-status-codes";
+import uploadToCloudinary from "../../../utils/uploader.js";
+import fs from "node:fs/promises";
 
 async function uploadImage(req, res, next) {
-  const image = await Image.create({ ...req.body });
+  try {
+    if (!req.file || !req.file.path) {
+      return next(
+        APIError.badRequest("Please provide a valid image file to upload."),
+      );
+    }
 
-  console.log(image);
-  res.status(201).json({ success: true, message: "upload successful" });
+    const { value, error } = imageSchemaValidation.validate({ ...req.body });
+
+    if (error) {
+      return next(APIError.badRequest(error?.details[0]?.message));
+    }
+
+    const { url, publicId } = await uploadToCloudinary(req.file.path);
+
+    await transactionsHelper(async (session) => {
+      const image = await Image.create([{ ...value, url, publicId }], session);
+
+      await fs.unlink(req.file.path);
+      return res
+        .status(StatusCodes.OK)
+        .json({ success: true, message: "upload successful", data: image[0] });
+    }, next);
+  } catch (error) {
+    await fs.unlink(req.file.path);
+    next(error);
+  }
 }
 
 export { uploadImage };
