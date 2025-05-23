86ab7bb2cf490ac3a1144e46a5c1014339e64a41
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-05-23 20:43:04 +0300

    style: api success responses

---
+++ src/utils/api-success-responses.js
@@ -0,0 +1,19 @@
+const API_SUCCESS_RESPONSES = {
+  image: {
+    UPDATED: "Image updated successfully",
+    IMG_UPLOAD: "Image uploaded successfully",
+  },
+  avatar: {
+    UPDATED: "Avatar updated successfully",
+  },
+
+  account: {
+    UPDATED: "Account updated successfully",
+    SIGN_UP: "Signup successfully",
+    SIGN_IN: "SignIn successfully",
+    SIGN_OUT: "Sign out successfully",
+    DETAILS: "Account details retrieved successfully",
+  },
+};
+
+export default API_SUCCESS_RESPONSES;
