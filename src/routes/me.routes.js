c5d402248bed609b9023bcd6563738b32c2bc57a
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-05-23 20:40:41 +0300

    feat(routing): auth user routes. defines authenticated user actions

--- src/routes/me.routes.js
+++ src/routes/me.routes.js
@@ -1,10 +1,37 @@
 import { Router } from "express";
 import { uploadImage } from "../controller/user/me/upload.controller.js";
+import upload from "../middleware/multer.middleware.js";
+import auth from "../middleware/auth.middleware.js";
+import {
+  editMe,
+  updateAvatar,
+  updateImage,
+} from "../controller/user/me/edit.controller.js";
+
+import normalizeArrayFields from "../middleware/normalize-array-fields.middleware.js";
 
 const meRouter = new Router();
 
-meRouter.get("/about-me");
-meRouter.post("/uploads", uploadImage);
-meRouter.post("/upload/avatar");
+meRouter.get("/about");
+
+meRouter.post(
+  "/uploads",
+  auth,
+  upload.single("image"),
+  normalizeArrayFields(["tags"]),
+  uploadImage,
+);
+
+meRouter.patch("/update", auth, editMe);
+
+meRouter.patch("/update-avatar", auth, upload.single("avatar"), updateAvatar);
+
+meRouter.patch(
+  "/uploads/:id",
+  auth,
+  upload.single("image"),
+  normalizeArrayFields(["tags"]),
+  updateImage,
+);
 
 export default meRouter;
