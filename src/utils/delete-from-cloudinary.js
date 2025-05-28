02a87f394bfab24f5539615f017bd6dd9063e872
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-05-28 22:29:35 +0300

    refactor: log error or success message to a file if deletion failed

--- src/utils/delete-from-cloudinary.js
+++ src/utils/delete-from-cloudinary.js
@@ -1,13 +1,25 @@
 import cloudinary from "../config/cloudinary.config.js";
-import APIError from "./ApiError.js";
+import logToFile from "./logger.js";
 
 async function deleteFromCloudinary(publicId) {
   try {
-    await cloudinary.uploader.destroy(publicId, { invalidate: true });
-  } catch (error) {
-    throw new APIError(
-      `Failed to delete image from CLOUDINARY : ${error.message}`,
-    );
+    const result = await cloudinary.uploader.destroy(publicId, {
+      invalidate: true,
+    });
+
+    if (result.result !== "ok") {
+      const message = `WARNING: cloudinary deletion failed for ${publicId}`;
+      console.warn(message);
+      await logToFile(message);
+    } else {
+      const message = `SUCCESS: image ${publicId} was deleted from cloudinary`;
+      console.log(message);
+      await logToFile(message);
+    }
+  } catch (cloudinaryError) {
+    const errMsg = `FATAL ERROR: cloudinary deletion error for ${publicId}:  ${cloudinaryError.message}`;
+    console.log(errMsg);
+    await logToFile(errMsg);
   }
 }
 
