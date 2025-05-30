0c7c7a4a80d3ace3e1422d59e84159290b6b8a0d
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-05-30 17:13:59 +0300

    refactor: handles jwt error & mongoose casterror

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -1,10 +1,16 @@
 import { StatusCodes } from "http-status-codes";
 
-const { BAD_REQUEST, CONFLICT } = StatusCodes;
+const { BAD_REQUEST, CONFLICT, UNAUTHORIZED } = StatusCodes;
 
 function globalErrorMiddleware(err, req, res, next) {
   let message = err.message || "Internal Server Error";
   let status = err.statusCode || 500;
+
+  // mongoose cast error
+  if (err.name === "CastError") {
+    message = "Wrong objectID, please provide a valid ID";
+    status = BAD_REQUEST;
+  }
 
   // mongoose duplicate key error
   if (err.code === 11000) {
@@ -13,7 +19,7 @@ function globalErrorMiddleware(err, req, res, next) {
     let msg = "";
 
     Object.keys(errObj).forEach((val) => {
-      const message = `${val} -> ${errObj[val]} already exists. Duplicate Value Error.`;
+      const message = `${val}  --- [ ${errObj[val]} ] already exists. Duplicate Value Error.`;
       msg += message;
     });
 
@@ -43,7 +49,12 @@ function globalErrorMiddleware(err, req, res, next) {
     }
   }
 
-  res.status(status).json({ success: false, message, err });
+  if (err.name === "JsonWebTokenError") {
+    message = "Invalid Token";
+    status = UNAUTHORIZED;
+  }
+
+  res.status(status).json({ success: false, message });
 }
 
 export default globalErrorMiddleware;
